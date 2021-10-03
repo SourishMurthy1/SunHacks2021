00dbb1b87adb8df2fcbf8ee3216c86f55848cd55
SourishMurthy1 <murthy.sourish@outlook.com>
2021-10-02 19:10:05 -0700

    FrontEnd Changes to LiveTranscript

--- frontend/src/Screens/LiveTranscript.jsx
+++ frontend/src/Screens/LiveTranscript.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import MicGIF from '../Assets/MicGIF.gif'
 
 const LiveTranscript = (props) => {
-    return ( 
-        <h1>Live Transcript</h1>
-     );
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>Live Transcription</h1>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img src={MicGIF} style={{ paddingTop: '10px', width: '100px' }} />
+            </div>
+            <div>
+                <textarea style={{paddingLeft: "10px", paddingTop: "10px", width: "1250px", height: "700px"}}>
+                    This is simple textarea
+                </textarea>
+            </div>
+        </div>
+    );
 }
- 
-export default LiveTranscript;
+
+export default LiveTranscript; 
