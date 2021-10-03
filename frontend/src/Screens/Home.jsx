2ad86c175b510b56d53ec644f03fa97382615d1c
SourishMurthy1 <murthy.sourish@outlook.com>
2021-10-02 19:10:34 -0700

    Other Functionality changes

--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { FilePicker } from 'react-file-picker'
+import ActionCards from '../Components/ScriptCards';
 const https = require('https')
 
 const Home = (props) => {
@@ -52,11 +53,9 @@ const Home = (props) => {
         //   req.end()
     }
 
-    
-
 return (
     <div>
-        <h1>Home</h1>
+        <h1 style={{textAlign: 'center'}}>Home</h1>
         <div>
             <button onClick={addFile}>upload File</button>
         </div>
@@ -72,8 +71,17 @@ return (
                     Upload Mp3 file
                 </button>
             </FilePicker> */}
-            
-
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+            <div style={{paddingRight: '10px'}}>
+                <ActionCards meeting='Meeting #1' date='10/2/21'/>
+            </div>    
+            <div style={{paddingRight: '10px'}}>
+                <ActionCards meeting='Meeting #2'date='9/30/21'/>
+            </div>
+            <div style={{paddingRight: '10px'}}>
+                <ActionCards meeting='Meeting #3'date='8/20/21'/>
+            </div>
+        </div>
     </div>
 );
 }
